2cfc6700842da0657303a9ff3b9d17e1d7133dee
Rocco Losito <roccolosito@gmail.com>
2020-07-25 22:16:39 -0400

    resorted file tree

--- Develop/public/server.js
+++
@@ -1,8 +0,0 @@
-// NPM package that we will use to give our server useful functionality
-const express = require("express");
-
-// Tells node that we are creating an "express" server
-const app = express();
-
-// Sets an initial port. We'll use this later in our listener
-const PORT = process.env.PORT || 8080;
